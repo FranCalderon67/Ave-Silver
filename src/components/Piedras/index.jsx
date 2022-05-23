bd7f385ae0c71d0c92d45c39c3d6e44ffa726a64
Cristianbenz <contactocbenitez@gmail.com>
2022-05-23 20:17:35 -0300

    css de lista de piedras y productos con responsive

--- src/components/Piedras/index.jsx
+++ src/components/Piedras/index.jsx
@@ -6,15 +6,17 @@ const Piedra = ({ item }) => {
   return (
     <>
       <div key={item.id} className="card mb-3">
-        <div className="row g-0">
-          <div className="col-md-4">
-            <img src={item.imagen} className="img-fluid rounded-start" alt="PIEDRAS AVE" />
+        <div className="row g-0 piedraCard--row">
+          <div className="col-md-4 piedraCard--imgContainer">
+            <img src={item.imagen} className="piedrasCard--img img-fluid" alt="PIEDRAS AVE" />
           </div>
-          <div className="col-md-8">
+          <div className="col-md-8 piedraCard--titleCol">
             <div className="card-body">
               <h5 className="card-title">{item.nombre}</h5>
-              <p className="card-text">{item.descripcion}</p>
             </div>
+          </div>
+          <div className="card-body row piedraCard--pCol">
+            <p className="card-text">{item.descripcion}</p>
           </div>
         </div>
       </div>
--- src/components/Producto/Index.jsx
+++ src/components/Producto/Index.jsx
@@ -1,18 +1,20 @@
 import React, { useState, useEffect } from "react";
+
 import { exportarProductos } from "../../productos";
+
 import "./producto.css";
 
 const Producto = ({ item }) => {
   return (
     <>
-      <div className="card " key={item.id}>
+      <div className="card" key={item.id}>
         <div>
           <img className="card-img-top" src={item.imagen} alt="ERROR" />
         </div>
         <div>
           <h5 className="card-title">{item.nombre}</h5>
         </div>
-        <p>Consultar</p>
+        <a href={`https://wa.me/5219841082450?text=Me%20interesa%20el%20prducto%20${item.nombre}%20de%20tu%20web`} target={'_blank'}>Consultar</a>
       </div>
     </>
   );
