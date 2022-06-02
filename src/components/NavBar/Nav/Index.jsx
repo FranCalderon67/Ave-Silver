dae03c86c8138a81e8f7718fe8e33b14a55b48ef
Cristianbenz <contactocbenitez@gmail.com>
2022-06-02 20:08:16 -0300

    icono dinamico de abrir/cerrar navBar

--- src/components/NavBar/Nav/Index.jsx
+++ src/components/NavBar/Nav/Index.jsx
@@ -11,9 +11,14 @@ export function Nav() {
   return (
     <>
       <div onClick={ () => setIsNavOpen(!isNavOpen) } className="nav--toggler-size nav--toggler--position nav--toggler-layout">
-        <span className="navToggler--span"></span>
-        <span className="navToggler--span"></span>
-        <span className="navToggler--span"></span>
+        {
+          !isNavOpen ? 
+          <img src={'https://i.ibb.co/qnG3pTq/menu.png'} alt='boton de abrir nav' />
+          :
+          <img src={'https://i.ibb.co/7bBymyG/close.png'} alt='boton de cerrar nav' />
+        }
+        
+        
       </div>
       <nav className={`navBar--nav-layout navBar--nav-size navBar--nav--position ${OPEN_NAV}`}>
           <ul className="nav--ul-layout">
